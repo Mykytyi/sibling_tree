a016181f67086822cb27ce8dd4a826f434eee0f3
Nikita <cherevaty.nik@gmail.com>
2019-05-19 16:10:21 +0300

    'solution2'

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 'use strict';
-
 const mainList = document.querySelector('.tree');
 
 mainList.addEventListener('click', function (event) {
-  let currentLi = event.target.parentNode;
-  if(currentLi.children[1]) {
-    currentLi.children[1].classList.toggle('display-none');
+  if(event.target.tagName === 'SPAN') {
+    let currentLi = event.target.parentNode;
+    if (currentLi.children[1]) {
+      currentLi.children[1].classList.toggle('display-none');
+    }
   }
 });
